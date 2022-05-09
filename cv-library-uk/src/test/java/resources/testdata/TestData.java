a23dc0ae3f66bc3bd1c3876a7e1f18a78894ed72
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-09 19:36:19 +0100

    Update TestData.java

--- cv-library-uk/src/test/java/resources/testdata/TestData.java
+++ cv-library-uk/src/test/java/resources/testdata/TestData.java
@@ -10,7 +10,9 @@ public class TestData {
                 {"tester","harrow","up to 5 miles","30000","500000","Per annum","Permanent","Permanent Tester jobs in Harrow on the Hill"},
                 {"Automation Tester","Sheffield","up to 5 miles","20000","40000","Per annum","Permanent","Permanent Automation Tester jobs in Sheffield"},
                 {"manager","Nottinghamshire","up to 15 miles","40000","45000","Per annum","Part Time","Part Time Manager jobs in Nottinghamshire"},
-                            {"nurse","derby","up to 10 miles","2200","3500","Per month","Contract","Contract Nurse jobs in Derby"}
+                            {"nurse","derby","up to 10 miles","2200","3500","Per month","Contract","Contract Nurse jobs in Derby"},
+                            {"Cleaner","london","up to 5 miles","100","200","Per day","Part Time","Part Time Cleaner jobs in London"}
+
 
              
         };return  data;
